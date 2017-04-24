3c98aca148d6325338e1d60b7507d3d0e015000a
user-pc <user-pc@DESKTOP-D5EV8V7>
2017-04-24 20:19:43 +0300

    added client service class

---
+++ STMS/Simple.TaskManagement.Client.Net/ClientService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.TaskManagement
+{
+    class ClientService : IClientService
+    {
+        public Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
