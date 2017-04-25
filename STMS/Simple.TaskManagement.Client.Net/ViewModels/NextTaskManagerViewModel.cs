670a06da27c248c7c34bea90c216c200804ad684
user-pc <user-pc@DESKTOP-D5EV8V7>
2017-04-25 15:28:10 +0300

    Next transaction factory, manager

---
+++ STMS/Simple.TaskManagement.Client.Net/ViewModels/NextTaskManagerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Reactive.Bindings;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Reactive.EventAggregator;
+using Reactive.Bindings.Notifiers;
+
+
+namespace Simple.TaskManagement.ViewModels
+{
+    public class NextTaskManagerViewModel : INotifyPropertyChanged
+    {
+        public ReactiveProperty<DateTimeOffset?> RequiredByDate { get; set; }
+        public ReactiveProperty<string> TaskDescription { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged = (_, __) => { };
+    }
+}
