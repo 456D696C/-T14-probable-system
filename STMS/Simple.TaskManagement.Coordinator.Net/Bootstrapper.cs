80f5bb84dc84d870b83fa65325a13af02eb601cf
user-pc <user-pc@DESKTOP-D5EV8V7>
2017-04-26 09:58:28 +0300

    refactored boostrapper, program

--- STMS/Simple.TaskManagement.Coordinator.Net/Bootstrapper.cs
+++ STMS/Simple.TaskManagement.Coordinator.Net/Bootstrapper.cs
@@ -12,6 +12,30 @@ using Simple.Query;
 using Simple.Logging;
 using Simple.TaskManagement.MessageHandlers;
 
+
+
+using Rebus.Auditing.Messages;
+using Rebus.Config;
+using Rebus.DataBus;
+using Rebus.DataBus.FileSystem;
+using Rebus.Logging;
+using Rebus.Persistence.FileSystem;
+using Rebus.Persistence.InMem;
+using Rebus.Routing.TypeBased;
+using Rebus.Transport.FileSystem;
+using Rebus.Unity;
+
+
+using Rebus.Sagas;
+using Rebus.Subscriptions;
+
+
+using Simple.TaskManagement.Common;
+
+
+using Simple.Rebus.Configuration;
+
+
 namespace Simple.TaskManagement.Coordinator
 {
     class Bootstrapper
@@ -19,13 +43,61 @@ namespace Simple.TaskManagement.Coordinator
         public static IUnityContainer RegisterTypes(IUnityContainer container)
         {
             container
-                .RegisterType<ISimpleLoggerFactory, ConsoleLoggerFactory>(new ContainerControlledLifetimeManager())
+                .RegisterType<ISimpleLoggerFactory,Simple.Logging.ConsoleLoggerFactory>(new ContainerControlledLifetimeManager())
                 .RegisterType<TaskCreatedSaga>()
                 .RegisterType<SendCompletionNotificationMessageHandler>()
                 .RegisterType<SendOverdueNotificationMessageHandler>()
                 ;
 
             return container;
+        }
+
+        public static IUnityContainer Start()
+        {
+            var container = Bootstrapper.RegisterTypes(new UnityContainer());
+            
+
+            DisplayContainerRegistrations(container);
+
+            var bus = 
+                Configure.With(new UnityContainerAdapter(container))
+               .Logging(l => l.ColoredConsole(minLevel: LogLevel.Debug))
+               .UseFilesystem(Config.FileSystem.BaseDirectory, Config.Queues.BackEnd)
+               .Options(b => b.EnableMessageAuditing(Config.Queues.AuditBackEnd))
+
+           .Start();
+
+
+            return container;
+        }
+
+        static void DisplayContainerRegistrations(IUnityContainer theContainer)
+        {
+            string regName, regType, mapTo, lifetime;
+            Console.WriteLine("Container has {0} Registrations:",
+                    theContainer.Registrations.Count());
+            foreach (ContainerRegistration item in theContainer.Registrations)
+            {
+                regType = item.RegisteredType.Name;
+                mapTo = item.MappedToType.Name;
+                regName = item.Name ?? "[default]";
+                lifetime = item.LifetimeManagerType.Name;
+                if (mapTo != regType)
+                {
+                    mapTo = " -> " + mapTo;
+                }
+                else
+                {
+                    mapTo = string.Empty;
+                }
+                lifetime = lifetime.Substring(0, lifetime.Length - "LifetimeManager".Length);
+                Console.WriteLine("+ {0}{1}  '{2}'  {3}", regType, mapTo, regName, lifetime);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+
         }
     }
 }
