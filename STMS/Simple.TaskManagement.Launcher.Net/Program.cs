30608d71068b97dd1e697c7c4af6a9b3ab2ee02c
user-pc <user-pc@DESKTOP-D5EV8V7>
2017-05-04 06:22:49 +0300

    launcher

---
+++ STMS/Simple.TaskManagement.Launcher.Net/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Simple.TaskManagement.Launcher
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (Run())
+            {
+                App.Main(); 
+            }
+        }
+
+
+        public static async Task Run()
+        {
+
+            await Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(7));
+                //
+                // Summary:
+                //     Closes a process that has a user interface by sending a close message to its
+                //     main window.
+                //
+                // Returns:
+                //     true if the close message was successfully sent; false if the associated process
+                //     does not have a main window or if the main window is disabled (for example if
+                //     a modal dialog is being shown).
+                //
+                if (!Process.GetCurrentProcess().CloseMainWindow())
+                    Process.GetCurrentProcess().Kill();
+
+                return true;
+
+            });
+
+        }
+
+
+        
+    }
+}
---
+++ STMS/Simple.TaskManagement.Launcher.NetTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple.TaskManagement.Launcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Simple.TaskManagement.Launcher;
+namespace Simple.TaskManagement.Launcher.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            var expected =Program.Run().Wait(TimeSpan.FromSeconds(17));
+            Assert.IsTrue(expected,"App killed from the task manager");
+        }
+    }
+}
