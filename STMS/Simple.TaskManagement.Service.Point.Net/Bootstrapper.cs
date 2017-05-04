56dada5ccdfb2b521722862b591c9cb1ecfca88f
user-pc <user-pc@DESKTOP-D5EV8V7>
2017-05-04 04:41:51 +0300

    null logger used, over verbose console

--- STMS/Simple.TaskManagement.Service.Point.Net/Bootstrapper.cs
+++ STMS/Simple.TaskManagement.Service.Point.Net/Bootstrapper.cs
@@ -56,7 +56,7 @@ namespace Simple.TaskManagement
         public static IUnityContainer RegisterTypes(IUnityContainer container)
         {
             container
-                .RegisterType<ISimpleLoggerFactory, Simple.Logging.ConsoleLoggerFactory>(new ContainerControlledLifetimeManager())
+                .RegisterType<ISimpleLoggerFactory, Simple.Logging.NullLoggerFactory>(new ContainerControlledLifetimeManager())
                 .RegisterType<ITaskStorage, InMemoryTaskStorageMockup>(new ContainerControlledLifetimeManager())
                 .RegisterType<IQueryProcessor, QueryProcessor>(nameof(QueryProcessor))
 
